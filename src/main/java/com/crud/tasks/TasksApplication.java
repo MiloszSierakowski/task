7b8aa398af9db1a3f864817d5d6f292a3ec805a0
Milosz <milosz.sierakowski@wp.pl>
2023-01-12 12:53:34 +0100

    23.3 Configuration to mail service

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,11 +1,13 @@
 package com.crud.tasks;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class TasksApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
