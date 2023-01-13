49743253e3a583f0d1062aad1cb19507f43ae337
Milosz <milosz.sierakowski@wp.pl>
2023-01-13 11:49:32 +0100

    23.3.9 fight with application.properties

--- src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
+++ src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Getter
 @Component
 public class TrelloConfig {
-    @Value("${trello.api.endpoint.prod:https://api.trello.com/1}")
+    @Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint;
     @Value("${trello.app.key}")
     private String trelloAppKey;
