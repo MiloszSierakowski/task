5caad79d0221f92b099316d7aef447a404368742
Milosz <milosz.sierakowski@wp.pl>
2023-03-20 23:02:05 +0100

    32.3 Task: Create an email template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -2,6 +2,8 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.config.CompanyConfig;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.thymeleaf.context.Context;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MailCreatorService {
@@ -17,12 +20,14 @@ public class MailCreatorService {
     private final CompanyConfig companyConfig;
     @Qualifier("templateEngine")
     private final TemplateEngine templateEngine;
+    private final TaskRepository taskRepository;
 
     @Autowired
-    public MailCreatorService(AdminConfig adminConfig, CompanyConfig companyConfig, TemplateEngine templateEngine) {
+    public MailCreatorService(AdminConfig adminConfig, CompanyConfig companyConfig, TemplateEngine templateEngine, TaskRepository taskRepository) {
         this.adminConfig = adminConfig;
         this.companyConfig = companyConfig;
         this.templateEngine = templateEngine;
+        this.taskRepository = taskRepository;
     }
 
     public String buildTrelloCardEmail(String message) {
@@ -49,5 +54,26 @@ public class MailCreatorService {
         context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
+    public String buildSecondTrelloCardEmail(String message) {
+
+        List<String> functionality = taskRepository.findAll().stream().map(Task::getContent).collect(Collectors.toList());
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://miloszsierakowski.github.io/");
+        context.setVariable("button", "Visit our website");
+        context.setVariable("gender", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("preview_message", "Aktualnie dostępne zadania");
+        context.setVariable("goodbye_message", "Do usłyszenia");
+        context.setVariable("company_name", "Nazwa Firmy " + companyConfig.getCompanyName());
+        context.setVariable("company_goal", "Nasze cele " + companyConfig.getCompanyGoal());
+        context.setVariable("company_email", "Email " + companyConfig.getCompanyEmail());
+        context.setVariable("company_phone", "Numer Telefonu " + companyConfig.getCompanyPhone());
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail2/created-second-trello-card-mail", context);
+    }
+
+
 
 }
