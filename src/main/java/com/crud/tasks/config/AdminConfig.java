d5d30e31d73a12aeac25b7cfe17ddd6ee41ffb3e
Milosz <milosz.sierakowski@wp.pl>
2023-03-20 10:35:10 +0100

    32.5 Bug fix on mogenius?

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Component;
 public class AdminConfig {
     @Value("${admin.mail}")
     private String adminMail;
-    @Value("${admin.name}")
-    private String adminName;
+//    @Value("${admin.name}")
+//    private String adminName;
 }
